c6f886cb6fd1350cb764974d635ac3be5eb3dfb6
stepankalensky <skalensky@gmail.com>
2018-03-15 15:43:40 +0100

    Moznost ukonceni hry

--- src/main/java/com/github/stepankalensky/adventura/ui/HomeController.java
+++ src/main/java/com/github/stepankalensky/adventura/ui/HomeController.java
@@ -1,8 +1,11 @@
 package com.github.stepankalensky.adventura.ui;
 
+
+
 import com.github.stepankalensky.adventura.logika.IHra;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -18,6 +21,7 @@ public class HomeController extends GridPane {
 	
 	@FXML private TextField textVstup;
 	@FXML private TextArea textVypis;
+    @FXML private Button odesli;
 	private IHra hra;
 	
 	/**
@@ -28,6 +32,15 @@ public class HomeController extends GridPane {
 		 String vypis = hra.zpracujPrikaz(textVstup.getText());
 		 textVypis.appendText("\n--------\n"+textVstup.getText()+"\n-------\n");
 		 textVypis.appendText(vypis);
+		 textVstup.setText("");
+		 
+		 if(hra.konecHry()) {
+			  textVypis.appendText("\n\n Konec hry \n");
+			  textVstup.setDisable(true);
+			  odesli.setDisable(true);
+			  
+			}
+
 		
 	}
 	public void inicializuj(IHra hra) {
